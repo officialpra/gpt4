9b1f8002f652444b54d61239ca3dc10fc682afbd
officialpra <prashantofficial.jain789@gmail.com>
2023-11-12 22:26:49 +0530

    first commit

---
+++ script2.js
@@ -0,0 +1,12 @@
+
+for(let i=1; i<=100; i++){
+    let newElement=document.createElement('p');
+    newElement.textContent="this is pra"+i;
+    document.body.appendChild(newElement);
+
+
+};
+for(let i=0; i<20; i++){
+    console.log("ok");
+
+}
